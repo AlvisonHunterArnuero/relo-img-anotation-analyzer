36bbb0287ed5bafc64ca491f761bac556836ba63
AlvisonHunterArnuero <alvison@gmail.com>
2025-08-22 02:20:07 -0600

    feat: Add FooterImgCarrousel component for displaying images in a carousel

---
+++ src/src/components/FooterImgCarrousel.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import React from 'react';
+import NextImage from 'next/image';
+import { Box, Container } from '@mui/material';
+import { Image } from '@/types/annotation';
+
+interface FooterImgCarrouselProps {
+  images: Image[];
+  onImageClick: (index: number) => void;
+}
+
+const FooterImgCarrousel: React.FC<FooterImgCarrouselProps> = ({
+  images,
+  onImageClick,
+}) => {
+  return (
+    <Container className="absolute bottom-0 overflow-hidden w-full">
+      <Box className="w-screen flex max-h-full gap-4 overflow-x-auto">
+        {images.map((img, idx) => (
+          <NextImage
+            key={img.id}
+            width={100}
+            height={60}
+            src={img.url}
+            onClick={() => onImageClick(idx)}
+            alt={`Footer Image ${img.id}`}
+            className="border-2 rounded-sm border-gray-300 shadow-md hover:cursor-pointer hover:scale-90 transform duration-500 ease-in-out"
+          />
+        ))}
+      </Box>
+    </Container>
+  );
+};
+
+export default FooterImgCarrousel;

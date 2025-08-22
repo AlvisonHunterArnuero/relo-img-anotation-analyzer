368024cecdfccaccbdbdb32626f5ebe7d5ac4614
AlvisonHunterArnuero <alvison@gmail.com>
2025-08-22 02:08:56 -0600

    refactor: Adds theme integration and layout enhancements
    Integrates Material-UI ThemeProvider and CssBaseline for consistent styling and design.
    
    Refactors layout by moving global styles to a reusable CSS class and adding a header section with title and description for improved UI structure and clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
 'use client';
 import ImageAnnotation from '@/src/components/ImageAnnotation';
-import { Paper } from '@mui/material';
+import {
+  Paper,
+  ThemeProvider,
+  CssBaseline,
+  Box,
+  Typography,
+} from '@mui/material';
+import theme from '../theme';
 
 export default function Home() {
   return (
-    <Paper className="pt-6 font-sans flex flex-col justify-center items-center w-full h-screen mx-auto container">
-      <ImageAnnotation />
-    </Paper>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box className="bg-gray-800 p-4">
+        <Typography
+          variant="h4"
+          component="h1"
+          className="text-white w-full mx-auto text-left pl-6 uppercasept-2"
+        >
+          Image Annotation Analyzer
+        </Typography>
+        <Typography
+          variant="body1"
+          className="text-gray-400 w-full mx-auto text-left pl-6 pt-2"
+        >
+          Annotate images with bounding boxes and categories.
+        </Typography>
+      </Box>
+      <Paper className="main-page">
+        <ImageAnnotation />
+      </Paper>
+    </ThemeProvider>
   );
 }
